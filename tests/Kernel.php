a4812bb76ec689b3a22638af9b88d98108636639
Christoph Reiter <reiter.christoph@gmail.com>
2023-05-16 14:42:56 +0200

    Remove references to deprecated classes

--- tests/Kernel.php
+++ tests/Kernel.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Dbp\Relay\CoreConnectorTextfileBundle\Tests;
 
-use ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle;
+use ApiPlatform\Symfony\Bundle\ApiPlatformBundle;
 use Dbp\Relay\CoreBundle\DbpRelayCoreBundle;
 use Dbp\Relay\CoreConnectorTextfileBundle\DbpRelayCoreConnectorTextfileBundle;
 use Nelmio\CorsBundle\NelmioCorsBundle;
